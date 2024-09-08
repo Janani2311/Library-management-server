9c5c4f1462b61911ae4d42559b3dece5abb029de
Janani2311 <156460993+Janani2311@users.noreply.github.com>
2024-09-08 17:54:13 +0530

    mongo_db URL

--- src/model/index.js
+++ src/model/index.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 import 'dotenv/config.js';
 
-mongoose.connect(`${process.env.MONGODB_URL}/${process.env.MONGODB_DB}`)
-.then((value)=>console.log("MongoDB Connected"))
+
+mongoose.connect(`${process.env.MONGODB_URL}`)
+.then((value)=>console.log("MongoDB Connected" ))
 .catch((err)=>console.log(err));
 
 export default mongoose;
