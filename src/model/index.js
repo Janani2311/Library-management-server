cc77f4cae75bc4dcdd0dded2fc349221870c1af6
Janani2311 <156460993+Janani2311@users.noreply.github.com>
2024-09-08 18:32:20 +0530

    db changes

--- src/model/index.js
+++ src/model/index.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 import 'dotenv/config.js';
 
 
-mongoose.connect(`${process.env.MONGODB_URL}`)
-.then((value)=>console.log("MongoDB Connected" ))
+mongoose.connect(`${process.env.MONGODB_URL}/${process.env.MONGODB_DB}`)
+.then((value)=>console.log("MongoDB Connected"+ `${process.env.MONGODB_URL}/${process.env.MONGODB_DB}`))
 .catch((err)=>console.log(err));
 
 export default mongoose;
